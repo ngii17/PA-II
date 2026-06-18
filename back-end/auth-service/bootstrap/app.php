f579af45a7ea90e3c371a6f2d8b7360ee72018fb
Ubuntu <ubuntu@ip-172-31-9-35.ap-southeast-1.compute.internal>
2026-06-18 06:25:48 +0000

    fix: tambah route avatars di nginx dan trustProxies di auth-service

--- back-end/auth-service/bootstrap/app.php
+++ back-end/auth-service/bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
